d40d7edfe60de9ace2c521be522fcd35f0000b72
Duy Nguyen <ngthduy90@gmail.com>
2018-03-26 22:16:46 +0700

    Finish chapter 6 in LearnNextjs website

--- NextExpressHelloworld/pages/index.js
+++ NextExpressHelloworld/pages/index.js
@@ -1,8 +1,22 @@
 import Layout from '../comps/BaseLayout'
+import Link from 'next/link'
+
+const PostLink = (props) => (
+  <li>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 const Index = () => (
   <Layout>
-    <p>Hello Duy</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js" />
+      <PostLink id="deploy-nextjs" title="Deploy Next.js" />
+    </ul>
   </Layout>
 );
 
