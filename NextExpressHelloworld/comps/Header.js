31b261fa323d22f9b6601df7e9e4b9bb7e557f69
Duy Nguyen <ngthduy90@gmail.com>
2018-03-27 23:54:44 +0700

    Add Ant Design lib into proj

--- NextExpressHelloworld/comps/Header.js
+++ NextExpressHelloworld/comps/Header.js
@@ -1,18 +1,51 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { Menu } from "antd";
+import "antd/dist/antd.css";
 
 const linkStyle = {
   marginRight: 15
 };
 
-const Header = () => (
-  <div>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
-  </div>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      current: ""
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    console.log(this);
+    console.log(e);
+
+    this.setState({
+      current: e.key
+    });
+  }
+
+  render() {
+    return (
+      <Menu
+        onClick={this.handleClick}
+        selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <Menu.Item key="home">
+          <Link href="/">
+            <a style={linkStyle}>Home</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="about">
+          <Link href="/about">
+            <a style={linkStyle}>About</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
 
 export default Header;
